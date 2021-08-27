cb3cb1e9abaf47b5e2f1192a85df074cbbaabf1f
Yaroslav Shlapak <yaroslav.shlapak@just-eat.com>
2021-08-27 20:01:20 +0300

    complete part 5

--- app/src/main/java/com/test/layoutsinjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/test/layoutsinjetpackcompose/MainActivity.kt
@@ -7,16 +7,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ContentAlpha
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
@@ -34,7 +39,7 @@ class MainActivity : ComponentActivity() {
             LayoutsInJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    PhotographerCard()
+                    LayoutsCodeLab()
                 }
             }
         }
@@ -66,9 +71,35 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
             Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
             // LocalContentAlpha is defining opacity level of its children
             CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                Text(text ="3 minutes ago", style = MaterialTheme.typography.body2)
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
             }
         }
+    }
+}
+
+@Composable
+fun LayoutsCodeLab() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "LayoutsCodelab") },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text("hi there!")
+        Text(text = "Thanks for going through the Layouts codelab")
     }
 }
 
@@ -76,6 +107,6 @@ fun PhotographerCard(modifier: Modifier = Modifier) {
 @Composable
 fun DefaultPreview() {
     LayoutsInJetpackComposeTheme {
-        PhotographerCard()
+        LayoutsCodeLab()
     }
 }
